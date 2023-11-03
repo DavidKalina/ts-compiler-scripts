4d55f98e5883b21e9c046da6a5f391ee55746af4
david@koptional <david@koptional.com>
2023-11-03 16:07:08 -0600

    feat: add categorization functions to the script. ignore node_modules

--- styleGuideChecker.ts
+++ styleGuideChecker.ts
@@ -6,6 +6,96 @@ const BASE_DIR = path.resolve("../../flowty/packages/web"); // Adjust this to yo
 
 const ASSET_RULE = /.*\.(jpg|jpeg|png|gif|svg|webp)$/;
 
+const ignoredDirectories = ["node_modules"];
+
+function shouldIgnore(filePath: string): boolean {
+  return ignoredDirectories.some((dir) => filePath.includes(dir));
+}
+
+const isUtil = (sourceFile: ts.SourceFile): boolean => {
+  let containsOnlyFunctions = true;
+  let containsReactComponent = false;
+  let utilFunctionFound = false;
+
+  const visit = (node: ts.Node): void => {
+    if (ts.isFunctionDeclaration(node)) {
+      utilFunctionFound = true;
+    }
+
+    // If the file contains a class or a JSX tag, consider it a React component
+    if (
+      ts.isClassDeclaration(node) ||
+      ts.isJsxOpeningElement(node) ||
+      ts.isJsxSelfClosingElement(node)
+    ) {
+      containsReactComponent = true;
+    }
+
+    if (
+      ts.isVariableDeclaration(node) &&
+      node.initializer &&
+      !ts.isFunctionExpression(node.initializer)
+    ) {
+      containsOnlyFunctions = false;
+    }
+
+    ts.forEachChild(node, visit);
+  };
+
+  ts.forEachChild(sourceFile, visit);
+
+  const fileName = sourceFile.fileName.toLowerCase();
+
+  return (
+    utilFunctionFound &&
+    containsOnlyFunctions &&
+    !containsReactComponent &&
+    !fileName.includes("service") &&
+    !isHook(sourceFile)
+  );
+};
+
+const isHook = (sourceFile: ts.SourceFile): boolean => {
+  let containsHookNaming = false;
+  let containsBuiltInHooks = false;
+
+  const visit = (node: ts.Node): void => {
+    // Check if the file has a function or variable declaration starting with "use"
+    if (
+      (ts.isFunctionDeclaration(node) || ts.isVariableDeclaration(node)) &&
+      node.name &&
+      ts.isIdentifier(node.name) &&
+      node.name.text.startsWith("use")
+    ) {
+      containsHookNaming = true;
+    }
+
+    // Check if the file uses built-in React hooks
+    if (ts.isCallExpression(node) && ts.isIdentifier(node.expression)) {
+      const functionName = node.expression.text;
+      if (
+        [
+          "useState",
+          "useEffect",
+          "useContext",
+          "useReducer",
+          "useCallback",
+          "useMemo",
+          "useRef",
+        ].includes(functionName)
+      ) {
+        containsBuiltInHooks = true;
+      }
+    }
+
+    ts.forEachChild(node, visit);
+  };
+
+  ts.forEachChild(sourceFile, visit);
+
+  return containsHookNaming && containsBuiltInHooks;
+};
+
 const isComponent = (sourceFile: ts.SourceFile): boolean => {
   let isComp = false;
 
@@ -79,12 +169,24 @@ const categorizeTSFile = (filePath: string): string => {
 
   if (!sourceFile) return "";
 
+  if (shouldIgnore(filePath)) {
+    return "";
+  }
+
   if (isContextFile(sourceFile)) {
     return "contexts";
   }
 
   if (isComponent(sourceFile)) {
     return "components";
+  }
+
+  if (isHook(sourceFile)) {
+    return "hooks";
+  }
+
+  if (isUtil(sourceFile)) {
+    return "util";
   }
   // Extend with more categorizations based on the file's content...
 
